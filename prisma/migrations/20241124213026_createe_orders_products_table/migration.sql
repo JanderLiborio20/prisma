497f15406f333ca853f01401177daaeee0421070
jander.liborio <jandernunes14@gmail.com>
2024-11-24 17:52:52 -0400

    create table pivo

---
+++ prisma/migrations/20241124213026_createe_orders_products_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" UUID NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders_products" (
+    "orderId" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "orders_products_pkey" PRIMARY KEY ("orderId","productId")
+);
+
+-- AddForeignKey
+ALTER TABLE "orders_products" ADD CONSTRAINT "orders_products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders_products" ADD CONSTRAINT "orders_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
